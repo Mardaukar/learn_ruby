7215479edcbbe318d6a3260ef91de9844c4dff22
Markus Huhtamäki <markus@seravo.fi>
2019-06-02 19:46:38 +0300

    One more exercise

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,18 @@
 #write your code here
+def add(first, second)
+  return first + second
+end
+
+def subtract(first, second)
+  return first - second
+end
+
+def sum(array)
+  sum = 0
+
+  for i in array
+    sum += i
+  end
+
+  return sum
+end
